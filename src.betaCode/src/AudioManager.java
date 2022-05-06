cc3d32bb1c7ef9c9b169f3a4a86841bc6696c4f9
Dalphan <danyventurini25@gmail.com>
2022-05-06 16:01:44 +0200

    audio manager

---
+++ src.betaCode/src/AudioManager.java
@@ -0,0 +1,35 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class AudioManager {
+    private static AudioManager instance;
+    private static Clip clip;
+
+    private AudioManager(){}
+
+    public static AudioManager getInstance(){
+        if (instance == null) instance = new AudioManager();
+        return instance;
+    }
+
+    public void play(String filename){
+        try {
+            /*InputStream in = new FileInputStream(filename);
+            AudioStream sound = new AudioStream(in);
+            AudioPlayer.player.start(sound);*/
+            if (clip != null && clip.isRunning()) clip.stop();
+
+            AudioInputStream stream = AudioSystem.getAudioInputStream(new File(filename));
+            clip = AudioSystem.getClip();
+            clip.open(stream);
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-20.0f); // Reduce volume by 10 decibels.
+            clip.start();
+        } catch (IOException | UnsupportedAudioFileException e1) {
+            e1.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src.betaCode/src/PrimoFrame.java
+++ src.betaCode/src/PrimoFrame.java
@@ -1,13 +1,65 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class PrimoFrame extends JFrame {
 
+    private JLabel label;
+    private JLabel image;
+
+    private JButton button;
+
     public PrimoFrame(){
         super("Prima finestra");
+        //Divide il frame in righe e colonne
+        setLayout(new GridBagLayout());
+
+        InitializeComponents();
+
         setSize(800, 800);
         setLocationRelativeTo(null);            //centra il frame
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private void InitializeComponents(){
+        //Components
+        label = new JLabel("Prova");
+
+        image = new JLabel(new ImageIcon("resources/images/White_deck/01.png"));
+
+        button = new JButton("Cliccami");
+        button.setFocusPainted(false);
+
+        //Listener
+        /*button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("riuscita");
+            }
+        });*/
+        button.addActionListener(e -> AudioManager.getInstance().play("resources/audio/rick roll.wav"));
+
+        //Layout
+        GridBagConstraints layout = new GridBagConstraints();
+        layout.gridx = 0;
+        layout.gridy = 0;
+        layout.weightx = 0;
+        layout.weighty = 0.01;
+        add(label, layout);
+
+        layout.gridx = 1;
+        layout.gridy = 0;
+        layout.weightx = 0;
+        layout.weighty = 0.01;
+        add(button, layout);
+
+        layout.gridx = 0;
+        layout.gridy = 1;
+        layout.weightx = 0;
+        layout.weighty = 1.0;
+        layout.gridwidth = 2;
+        layout.gridheight = 1;
+        add(image, layout);
     }
 }
