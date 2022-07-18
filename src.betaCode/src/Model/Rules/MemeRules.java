59bff2db7de2c4fb867da475c1af055be9d197ed
luigi_dannibale <luigi.dannibale02@gmail.com>
2022-07-18 19:13:29 +0200

    Update MemeRules.java

--- src.betaCode/src/Model/Rules/MemeRules.java
+++ src.betaCode/src/Model/Rules/MemeRules.java
@@ -1,8 +1,14 @@
 package Model.Rules;
 
 import Model.Cards.Card;
+import Model.Cards.CardBuilder;
+import Model.Cards.DrawCard;
+import Model.Cards.ReverseCard;
 import Model.Deck;
+import Model.Enumerations.CardColor;
 import Model.Enumerations.CardValue;
+import Model.Interfaces.SkipAction;
+import Model.Interfaces.WildAction;
 import Model.Player.Player;
 import Model.TurnManager;
 
@@ -27,11 +33,32 @@ public class MemeRules extends UnoGameRules{
 
     @Override
     public List<Card> getPlayableCards(List<Card> playerPlayableHand, Card discardsPick) {
+        if (discardsPick instanceof SkipAction || discardsPick instanceof WildAction )
+        {
+            switch (discardsPick.getValue())
+            {
+                case WILD_DRAW -> {}
+                case SKIP -> {}
+                case DRAW -> {playerPlayableHand.forEach(card -> card.getValue()==CardValue.DRAW?:playerPlayableHand.remove(card));}
+                default ->{}
+            }
 
+        }
         return playerPlayableHand;
     }
 
-    public void cardActionPerformance(TurnManager turnManager, Card cardToPlay, Player[] players, Deck deck){
-
-    }
+    @Override
+    public void cardActionPerformance(TurnManager turnManager, Card card, Player[] players, Deck deck)
+    {
+        if (turnManager.getLastCardPlayed() instanceof WildAction && turnManager.getLastCardPlayed().getColor() == CardColor.WILD) {
+            CardColor c = ((WildAction) turnManager.getLastCardPlayed()).changeColor();
+            turnManager.updateLastCardPlayed(CardBuilder.createFlowCard(c, CardValue.WILD));
+        }
+        if(turnManager.getLastCardPlayed() instanceof DrawCard)
+            players[turnManager.next()].drawCards(deck.draw(((DrawCard) turnManager.getLastCardPlayed()).getNumberOfCardsToDraw()));
+        else if(turnManager.getLastCardPlayed() instanceof ReverseCard)
+            ((ReverseCard) turnManager.getLastCardPlayed()).reverse(turnManager, players);
+        if(turnManager.getLastCardPlayed() instanceof SkipAction)
+            ((SkipAction) turnManager.getLastCardPlayed()).skipTurn(turnManager);
+    };
 }
