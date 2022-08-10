a8324eb432f6f36fd73265430f9ab843d4dd87a3
luigi_dannibale <luigi.dannibale02@gmail.com>
2022-08-10 18:47:23 +0200

    Create ChangebleIcon.java

---
+++ src.betaCode/src/View/ChangebleIcon.java
@@ -0,0 +1,17 @@
+package View;
+
+import javax.swing.*;
+import java.util.HashMap;
+
+public class ChangebleIcon extends JLabel{
+    public HashMap<String,Icon> ICONS= new HashMap<>();
+    public ChangebleIcon(String path, String[]fileNames,String fileExtension)
+    {
+        for(String fileName:fileNames)
+            ICONS.put(fileName,new ImageIcon(path+fileName+fileExtension));
+    }
+
+    public void setIcon(String iconID) {
+        super.setIcon(ICONS.get(iconID));
+    }
+}
