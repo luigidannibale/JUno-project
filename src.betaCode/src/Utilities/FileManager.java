2d871a1734adeb772d0afc24b4cf3f293b9fdd15
luigi_dannibale <luigi.dannibale02@gmail.com>
2022-08-23 14:26:32 +0200

    update

--- src.betaCode/src/Utilities/FileManager.java
+++ src.betaCode/src/Utilities/FileManager.java
@@ -14,13 +14,13 @@ import java.util.HashMap;
 
 public class FileManager
 {
-    public boolean writeSingleJson(HashMap<String,Object> data, String File)
+    public boolean writeSingleJson(HashMap<String,Object> data, String file)
     {
         JSONObject dataJson = new JSONObject();
         data.forEach(dataJson::put);
-        return write(dataJson.toString(), File);
+        return write(dataJson.toString(), file);
     }
-    public boolean writeJson(HashMap<String,Object> datas[], String File)
+    public boolean writeJson(HashMap<String,Object> datas[], String file)
     {
         JSONArray array = new JSONArray();
         for (HashMap<String,Object> data:datas)
@@ -29,14 +29,14 @@ public class FileManager
             data.forEach(dataJson::put);
             array.put(dataJson);
         }
-        return write(array.toString(), File);
+        return write(array.toString(), file);
     }
-    private boolean write(String data, String File)
+    private boolean write(String data, String file)
     {
-        try (FileWriter file = new FileWriter(File))
+        try (FileWriter fw = new FileWriter(file))
         {
-            file.write(data);
-            file.flush();
+            fw.write(data);
+            fw.flush();
         }
         catch (IOException e)
         {
@@ -45,18 +45,23 @@ public class FileManager
         }
         return true;
     }
-    private boolean read(JSONArray data, String File)
+    public HashMap<String,Object>  readJson(String file) throws IOException
     {
-        try(BufferedReader br = new BufferedReader(new FileReader(File)))
+        JSONArray datasJson = read(file);
+        HashMap<String,Object> data = new HashMap<>();
+        datasJson.forEach();
+    }
+    private JSONArray read(String file) throws IOException
+    {
+        try(BufferedReader br = new BufferedReader(new FileReader(file)))
         {
-            data = new JSONArray(br.lines());
+            return new JSONArray(br.lines());
             //br.lines().forEach(data::put);
         }
         catch (IOException e)
         {
             e.printStackTrace();
-            return false;
+            throw e;
         }
-        return true;
     }
 }
