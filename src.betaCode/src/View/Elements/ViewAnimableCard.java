a918b937025399a9c0f93abca9de0e42d314de88
luigi_dannibale <luigi.dannibale02@gmail.com>
2022-08-29 22:56:29 +0200

    update

---
+++ src.betaCode/src/View/Elements/ViewAnimableCard.java
@@ -0,0 +1,19 @@
+package View.Elements;
+
+import Model.Cards.Card;
+
+import java.awt.*;
+
+public class ViewAnimableCard extends ViewRotatableImage
+{
+    private Dimension shiftAmount = new Dimension(0,0);
+
+    public ViewAnimableCard(Card c, int rotation) { super(c,rotation); }
+    public double getShiftHeight() { return shiftAmount.getHeight(); }
+
+    public double getShiftWidth() { return shiftAmount.getWidth(); }
+    public void setShiftHeight(double shiftHeight) { shiftAmount.setSize(shiftAmount.getWidth(),shiftHeight); }
+
+    public void setShiftWidth(double shiftWidth) { shiftAmount.setSize(shiftWidth,shiftAmount.getHeight()); }
+
+}
---
+++ src.betaCode/src/View/Elements/ViewRotatableImage.java
@@ -0,0 +1,9 @@
+package View.Elements;
+
+import Model.Cards.Card;
+
+public class ViewRotatableImage extends ViewCard
+{
+    public ViewRotatableImage(Card c, int rotation)  { super(c,rotation); }
+
+}
